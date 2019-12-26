4cd270a490576d544dd8ecfbed3ea22c819e6908
SanLi <2689170096@qq.com>
2019-12-26 10:56:29 +0800

    :zap: 优化代码

--- src/main/java/cn/smallbun/scaffold/manage/service/impl/SysGroupServiceImpl.java
+++ src/main/java/cn/smallbun/scaffold/manage/service/impl/SysGroupServiceImpl.java
@@ -16,14 +16,22 @@
 
 package cn.smallbun.scaffold.manage.service.impl;
 
+import cn.smallbun.scaffold.framework.context.ApplicationContextHelp;
 import cn.smallbun.scaffold.framework.mybatis.domain.IdEntity;
 import cn.smallbun.scaffold.framework.mybatis.service.BaseServiceImpl;
 import cn.smallbun.scaffold.manage.entity.SysGroupEntity;
+import cn.smallbun.scaffold.manage.entity.SysUserEntity;
 import cn.smallbun.scaffold.manage.enums.GroupStatus;
 import cn.smallbun.scaffold.manage.mapper.SysGroupMapper;
 import cn.smallbun.scaffold.manage.service.ISysGroupService;
+import cn.smallbun.scaffold.manage.service.ISysUserService;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import org.springframework.stereotype.Service;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.Objects;
 
 /**
  * <p>
@@ -35,6 +43,7 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class SysGroupServiceImpl extends BaseServiceImpl<SysGroupMapper, SysGroupEntity> implements ISysGroupService {
+
 
 	/**
 	 * 根据ID更新组织机构信息
@@ -49,5 +58,30 @@ public class SysGroupServiceImpl extends BaseServiceImpl<SysGroupMapper, SysGrou
 				.eq(IdEntity::getId, id)
 				// 设置装填
 				.set(SysGroupEntity::getStatus, status));
+	}
+
+	/**
+	 * 删除组织
+	 * @param idList {@link Collection} id
+	 * @return boolean
+	 */
+	@Override
+	public boolean removeByIds(Collection<? extends Serializable> idList) {
+		idList.forEach(id -> {
+			//查询是否具有关联关系，如有有不能删除
+			LambdaQueryWrapper<SysUserEntity> condition = new LambdaQueryWrapper<SysUserEntity>()
+					.eq(SysUserEntity::getGroupId, id);
+			//查询总数
+			int count = ApplicationContextHelp.getBean(ISysUserService.class).count(condition);
+			if (count > 0) {
+				//查询机构信息
+				SysGroupEntity group = getById(id);
+				if (!Objects.isNull(group)) {
+					//抛出异常
+					throw new RuntimeException(group.getName() + "下存在[" + count + "]条记录，请处理后在进行删除操作！");
+				}
+			}
+		});
+		return super.removeByIds(idList);
 	}
 }
